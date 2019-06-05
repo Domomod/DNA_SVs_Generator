a118208d983ad0eab54b04489cea0b851dc1e0b5
Krzysztof Sychla <ksychla5@gmail.com>
2019-06-05 20:22:59 +0200

    Working methods

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from random import randint
 
 
 def main():
-    def inser(genome, vs_ref_table, vs_new_table):
+    def inser(pos_from, pos_to, size):
         pass
 
-    def trans(genome, vs_ref_table, vs_new_table):
-        pass
+    def trans(pos_start, pos_end, size):
+        nonlocal full_read, offset_to_ref, vs_ref, vs_new
+        dupli(pos_start, pos_end, size)
+        dele(pos_start, size)
 
     def dele(pos, size):
         nonlocal full_read, offset_to_ref, vs_ref, vs_new
@@ -16,20 +18,33 @@ def main():
         vs_new.append(("deletion", pos, pos + size))
         offset_to_ref += size
 
-    def inver(genome, vs_ref_table, vs_new_table):
-        pass
+    def inver(pos, size):
+        nonlocal full_read, offset_to_ref, vs_ref, vs_new
+        for i in range(int(size / 2) + 1):
+            full_read[i + pos], full_read[pos+size - i] = full_read[pos+size - i], full_read[i + pos]
+        vs_ref.append(("inversion", pos + offset_to_ref, pos + offset_to_ref + size))
+        vs_new.append(("inversion", pos, pos + size))
 
-    def dupli(genome, vs_ref_table, vs_new_table):
-        pass
+    def dupli(pos_start, pos_end, size):
+        nonlocal full_read, offset_to_ref, vs_ref, vs_new
+        for i in range(size):
+            full_read.insert(pos_end+i, full_read[pos_start+i])
+        # vs_ref.append(("dupl", pos + offset_to_ref, pos + offset_to_ref + size)) # ??
+        # vs_new.append(("dupl", pos, pos + size)) # ??
 
-    def dupli_t(genome, vs_ref_table, vs_new_table):
-        pass
+    def dupli_t(pos, size):
+        nonlocal full_read, offset_to_ref, vs_ref, vs_new
+        for i in range(size):
+            full_read.insert(pos+size+i, full_read[pos+i])
+        vs_ref.append(("tandem_dupl", pos + offset_to_ref, pos + offset_to_ref + size)) # ??
+        vs_new.append(("tandem_dupl", pos, pos + size)) # ??
+
 
     chromosome_file_path = "S288C_ref/"
     chromosome_names = []
     for i in range(1, 2):
         chromosome_names.append("chr{:02d}.fsa".format(i))
-    chromosome_names.append("chrmt.fsa".format(i))
+        chromosome_names.append("chrmt.fsa".format(i))
 
     test_file = open("test", "w")
 
